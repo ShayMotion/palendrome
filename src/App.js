e8def419c2d8ae425f48210312c81a9fa13052d1
Shayan Mohsin <shaymotionfilms@gmail.com>
2021-06-23 08:24:30 -0400

    css

--- src/App.js
+++ src/App.js
@@ -3,9 +3,12 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <input />
-      <button></button>
-      <div>Palendrome App</div>
+      <div className="main">
+        <div className="inputArea"></div>
+        <input />
+        <button>Submit</button>
+        <div>Palendrome App</div>
+      </div>
     </div>
   );
 }
