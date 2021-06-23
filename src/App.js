c2d9502e75389707a53e866dd3f5601a2176c9ce
Shayan Mohsin <shaymotionfilms@gmail.com>
2021-06-23 08:16:42 -0400

    new app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <input />
+      <button></button>
+      <div>Palendrome App</div>
+    </div>
+  );
+}
+
+export default App;
